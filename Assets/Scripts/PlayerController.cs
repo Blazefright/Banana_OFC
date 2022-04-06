eaa6a43cbd8ea7df2062421e0a8d21ab281be21d
Alex Vulpx <DylanR.Sawyer@outlook.com>
2022-04-06 13:20:55 -0400

    AN 01
    Animation updates

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -19,10 +19,19 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     GameObject flagPos;
 
+    private Animator playerAN;
+
+    private SpriteRenderer playerSR;
+
+    [SerializeField]
+    int maxIdleTimer = 1;
+
     private bool canRespawn = true;
 
     void Start()
     {
+        playerSR = GetComponent<SpriteRenderer>();
+        playerAN = GetComponent<Animator>();
         playerRB = GetComponent<Rigidbody2D>();
         playerRB.transform.position = startingPos.transform.position;
     }
@@ -40,23 +49,74 @@ public class PlayerController : MonoBehaviour
             playerRB.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
 
             ableJump = false;
+            playerAN.SetBool("OnGround", false);
+
         }
         if (Input.GetKey(KeyCode.A))
         {
             playerRB.AddForce(Vector2.left * speed, ForceMode2D.Force);
+            playerAN.SetBool("Idle", false);
+            playerAN.SetBool("Walking", true);
+            playerSR.flipX = true;
         }
-
-        if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D))
         {
             playerRB.AddForce(Vector2.right * speed, ForceMode2D.Force);
+            playerAN.SetBool("Idle", false);
+            playerAN.SetBool("Walking", true);
+            playerSR.flipX = false;
         }
+        else
+        {
+            playerAN.SetBool("Walking", false);
+            playerAN.SetBool("Idle", true);
+        }
+
+        if (playerRB.velocity.y > 0.1f)
+        {
+            playerAN.SetBool("Idle", false);
+            playerAN.SetBool("JumpUp", true);
+            playerAN.SetBool("Falling", false);
+        }
+        else if (playerRB.velocity.y < -0.1f)
+        {
+            playerAN.SetBool("JumpUp", false);
+            playerAN.SetBool("Falling", true);
+            playerAN.SetBool("Idle", false);
+        }
+        else
+        {
+            playerAN.SetBool("JumpUp", false);
+            playerAN.SetBool("Falling", false);
+        }
+
+        //if (playerAN.GetBool("Idle") == true)
+        //{
+        //    StartCoroutine(Sittertimer());
+            
+        //}
     }
+
+    //IEnumerator Sittertimer()
+    //{
+    //    if (maxIdleTimer < 5)
+    //    {
+    //        maxIdleTimer = +1;
+            
+    //    }
+
+    //    yield return new WaitForSecondsRealtime(1);
+    //}
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground") && !ableJump)
         {
             ableJump = true;
+
+            
+            playerAN.SetBool("JumpUp", false);
+            playerAN.SetBool("OnGround", true);
         }
         if (collision.gameObject.CompareTag("StaticBad") && canRespawn)
         {
--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 <!-- Banana Of Fucking Course-->
 ### By 
 ## LowFi_Overwrite
- 
-Starting to get semi good! You can now play the basic thing but it isn't the best.
-> March 31st, 2022
+
+> Animations in Poggers!
+- 4/6/2022
