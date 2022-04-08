c533f600a41ab7b9de38dc63deeecca10b71e2bf
Alex Vulpx <DylanR.Sawyer@outlook.com>
2022-04-08 12:29:17 -0400

    Stumped but close
    assEater

--- Assets/Scripts/StaticEnemies.cs
+++ Assets/Scripts/StaticEnemies.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class StaticEnemies : MonoBehaviour
 {
+    private Rigidbody2D assEater;
 
     [SerializeField]
     Vector2[] jackArray;
+
+    [SerializeField]
+    int howManyjack;
+
     void Start()
     {
-        
+        assEater = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        
+       
     }
 }
