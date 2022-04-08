72eba52f6bdc99221d0edc164abe42a28bee1da4
Alex Vulpx <DylanR.Sawyer@outlook.com>
2022-04-08 11:34:09 -0400

    started enemy scripts
    started a vector 2 array to make the enemies move through them at a set pace

---
+++ Assets/Scripts/StaticEnemies.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticEnemies : MonoBehaviour
+{
+
+    [SerializeField]
+    Vector2[] jackArray;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+}
